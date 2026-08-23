ec86f8efe9562dd9a5d0d0e051a66cd989fdea93
André Luiz Duarte <andre.duarte@edu.unifil.br>
2026-08-23 05:46:06 -0300

    Implementação DoublyLinkedList com métodos de inserção e deletar

---
+++ DoublyLinkedList.java
@@ -0,0 +1,90 @@
+public class DoublyLinkedList {
+    Node begin;
+    Node end;
+    int size;
+
+    public DoublyLinkedList() {
+        this.begin = null;
+        this.end = null;
+        this.size = 0;
+    }
+
+
+    public void insertValue(int value) {
+        Node newNode = new Node(value);
+
+        if (begin == null) {
+
+            begin = newNode;
+            end = newNode;
+        } else {
+
+            end.next = newNode;
+            newNode.previous = end;
+            end = newNode;
+        }
+        size++;
+    }
+
+
+    public boolean deleteValue(int value) {
+        if (begin == null) {
+            return false; // Lista vazia
+        }
+
+        Node currentNode = begin;
+
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+
+
+                if (currentNode == begin && currentNode == end) {
+                    begin = null;
+                    end = null;
+                }
+
+                else if (currentNode == begin) {
+                    begin = currentNode.next;
+                    begin.previous = null;
+                }
+
+                else if (currentNode == end) {
+                    end = currentNode.previous;
+                    end.next = null;
+                }
+
+                else {
+                    currentNode.previous.next = currentNode.next;
+                    currentNode.next.previous = currentNode.previous;
+                }
+
+                size--;
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return false;
+    }
+
+    public void printList() {
+        Node currentNode = begin;
+
+        if (currentNode == null) {
+            System.out.println("Lista vazia");
+            return;
+        }
+
+        while (currentNode != null) {
+            System.out.print(currentNode.value);
+            if (currentNode.next != null) {
+                System.out.print(" <~> ");
+            }
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+    public int getSize() {
+        return this.size;
+    }
+}
